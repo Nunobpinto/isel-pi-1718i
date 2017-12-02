267de879ed5aeb9551ca6d443be198c110d79a72
JpmGameiro <41893@alunos.isel.ipl.pt>
2017-12-02 23:36:44 +0000

    concluded userService documentation

--- serie2/domain/service/userService.js
+++ serie2/domain/service/userService.js
@@ -7,6 +7,10 @@ const debug = require('debug')('serie2:userService')
 
 const url = global.couchdb_url + '/users/'
 
+/**
+ * Defines req according to dataSource
+ * @param dataSource
+ */
 function init(dataSource) {
 	let req
 	if( dataSource )
@@ -21,6 +25,12 @@ function init(dataSource) {
 		findById
 	}
 
+	/**
+	 * Get user by username and password in order to login
+     * @param username
+     * @param password
+     * @param cb
+     */
 	function getUser(username, password, cb) {
 		debug('Fetching user with id = ' + username)
 		req(utils.optionsBuilder('GET', url + username), (err, res, body) => {
@@ -31,6 +41,14 @@ function init(dataSource) {
 		})
 	}
 
+	/**
+	 * Create user with given params
+     * @param username
+     * @param password
+     * @param fullName
+     * @param email
+     * @param cb
+     */
 	function createUser(username, password, fullName, email, cb) {
 		debug('Creating user with username = ' + username)
 		const json = {
@@ -47,6 +65,11 @@ function init(dataSource) {
 		})
 	}
 
+	/**
+	 * Delete user received in param
+     * @param user
+     * @param cb
+     */
 	function deleteUser(user, cb) {
 		debug('Deleting user with id = ' + user.username)
 		req(utils.optionsBuilder('DELETE', url + user.username + `?rev=${user._rev}`), (err) => {
@@ -55,6 +78,11 @@ function init(dataSource) {
 		})
 	}
 
+	/**
+	 * Find the user with de id received in param
+     * @param username
+     * @param cb
+     */
 	function findById(username, cb) {
 		req(utils.optionsBuilder('GET', url + username), (err, res, body) => {
 			if ( err || res.statusCode === 404 ) return cb( { message: 'Something broke!', statusCode: (res ? res.statusCode : 500) } )
