217a27e331c0995436bfa0198d87532e57c28295
brunofilipe <bruno.filipe1996@hotmail.com>
2017-12-22 21:18:57 +0000

    deleting user list functionality added

--- public/javascripts/list.js
+++ public/javascripts/list.js
@@ -1,31 +1,30 @@
-window.onload = function () {
 
-    function removeList(listId, username) {
-        const path = `/users/${{username}}/lists`
-        const data = `listId=${listId}`
-        httpRequest('DELETE', path, data, err => {
-            if (err) return alert(err.message)
-            const mainDiv = document.getElementsByName('mainDiv')
-            const divToRemove = document.getElementsByName(`list-${listId}`)
-            mainDiv.removeChild(divToRemove)
-        })
-    }
 
-    function httpRequest(method, path, data, cb) {
-        const xhr = new XMLHttpRequest()
-        xhr.open(method, path, true)
+function removeList(listId, username) {
+	const path = `/users/${username}/lists`
+    const data = `listID=${listId}`
+    httpRequest('DELETE', path, data, err => {
+        if (err) return alert(err.message)
+        const mainDiv = document.getElementById('mainDiv')
+        const divToRemove = document.getElementById(`list-${listId}`)
+        mainDiv.removeChild(divToRemove)
+    })
+}
 
-        //Send the proper header information along with the request
-        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+function httpRequest(method, path, data, cb) {
+    const xhr = new XMLHttpRequest()
+    xhr.open(method, path, true)
 
-        xhr.onreadystatechange = function () {//Call a function when the state changes.
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status === 200)
-                    cb()
-                else
-                    cb(new Error(xhr.status + ': ' + xhr.responseText))
-            }
+    //Send the proper header information along with the request
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+
+    xhr.onreadystatechange = function () {//Call a function when the state changes.
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200)
+                cb()
+            else
+                cb(new Error(xhr.status + ': ' + xhr.responseText))
         }
-        xhr.send(data)
     }
+    xhr.send(data)
 }
